174addbc3916395ad4f9433a874f82fcd58fcd1a
deepvn98 <deepvn988@gmail.com>
2021-09-18 20:45:25 +0700

    material git status

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+const MaterialComponent = 
+[MatButtonModule, MatButtonToggleModule];
+
+@NgModule({
+  imports: [MaterialComponent],
+  exports: [MaterialComponent],
+})
+export class MaterialModule {}
